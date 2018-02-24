ebb262003d3496c3c741963bfc49c469a3696090
Cernan Bernardo <cernanb@gmail.com>
2018-02-23 23:12:15 -0800

    Build Account dumb component

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-export default props => (
-  <div>
-    <h2>Account</h2>
-  </div>
-)
+export default ({ account }) => {
+  return (
+    <div>
+      <h2>{account.financialInst}</h2>
+      <h5>{account.accountType}</h5>
+      <h3>{account.balance}</h3>
+    </div>
+  )
+}
