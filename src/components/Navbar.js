43bbd7d15367dd56c7cfc08ed84b458440991d7f
Cernan Bernardo <cernanb@gmail.com>
2018-02-24 07:43:24 -0800

    Style navbar li

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div style={{ backgroundColor: 'lightblue' }}>Navbar</div>
+  return (
+    <ul
+      style={{
+        backgroundColor: 'lightblue',
+        marginTop: 0,
+        marginBottom: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 0,
+      }}
+    >
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+    </ul>
+  )
 }
 export default Navbar
